4ece8b5297103a4a5482e152a9c7786097b59f2e
AndreasSamuelsson <andsamuppsala@gmail.com>
2022-01-25 17:31:42 +0100

    fix name lenght and overflow

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Home: NextPage = () => {
 
   const data = [
     {
-      name: 'Värde 1',
+      name: 'Databasadministratör',
       num: 132,
       forecast: 243,
     },
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
     <article className="max-w-6xl flex flex-col min-h-screen mx-auto pt-16">
     <h2 className="text-4xl">Översikt</h2>
     <h3 className="text-2xl py-8">Info</h3>
-    <div className="flex flex-col lg:flex-row justify-between">
+    <div className="flex flex-col xl:flex-row justify-between">
       <div className="flex flex-col">
       <h3 className="text-2xl py-4">Topplista kompetenser</h3>
         <Toplist data={data} title="Namn"></Toplist></div>
