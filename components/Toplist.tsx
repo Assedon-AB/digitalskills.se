8ba8439e2472ff8afff8d45ac20f1325ba01e81b
Elias Juremalm <eliass.juremalm@gmail.com>
2022-02-01 09:51:37 +0100

    complete statscard and searchbar with mockup data

--- components/Toplist.tsx
+++ components/Toplist.tsx
@@ -1,139 +1,147 @@
-
 import { reverse } from "dns";
 import Link from "next/link";
 import { useState } from "react";
 import { setEnvironmentData } from "worker_threads";
 import FilterDropdown from "./filterDropdown";
 import ToplistRow from "./ToplistRow";
-import {SortAscendingIcon} from '@heroicons/react/solid';
+import { SortAscendingIcon } from "@heroicons/react/solid";
 
 interface ToplistProps {
-    data: {
-    name: string,
-      num: number,
-      forecast: number,
-    }[];
-    title: string,
-    category: string,
-  
-  }
+  data: {
+    name: string;
+    num: number;
+    forecast: number;
+  }[];
+  title: string;
+  category: string;
+}
 
-const Toplist = ({ data, title , category}: ToplistProps) => {
-  const [sortMode, setSortMode] = useState('Antal annonser')
-  const [showMode, setShowMode] = useState('Prognos 12 mån')
-  
+const Toplist = ({ data, title, category }: ToplistProps) => {
+  const [sortMode, setSortMode] = useState("Antal annonser");
+  const [showMode, setShowMode] = useState("Prognos 12 mån");
+
   const changeData = (arg: string) => {
     setShowMode(arg);
-    setSortMode('Antal annonser')
+    setSortMode("Antal annonser");
   };
-  
+
   function sortBy(arr: any[], mode: string, showWhat: string) {
-    console.log(mode)
-    var prop = 'num'
-    var show = 'forecast'
-    var filteredList: {'name': string, 'num': number, 'data': number}[] = []
-    if(mode == "Antal annonser") {prop = 'num'}
-    else if(mode == "Prognos 12 mån") {prop = "forecast"}
-    else if(mode == "Trend 12 mån") {prop = "trend"}
-    if(showWhat == "Prognos 12 mån") {show = "forecast"}
-    else if(showWhat == "Trend 12 mån") {show = "trend"}
+    console.log(mode);
+    var prop = "num";
+    var show = "forecast";
+    var filteredList: { name: string; num: number; data: number }[] = [];
+    if (mode == "Antal annonser") {
+      prop = "num";
+    } else if (mode == "Prognos 12 mån") {
+      prop = "forecast";
+    } else if (mode == "Trend 12 mån") {
+      prop = "trend";
+    }
+    if (showWhat == "Prognos 12 mån") {
+      show = "forecast";
+    } else if (showWhat == "Trend 12 mån") {
+      show = "trend";
+    }
     data = arr.sort((a, b) => b[prop] - a[prop]);
     for (const index in data) {
-      filteredList.push({'name': arr[index]['name'], 'num': arr[index]['num'], 'data': arr[index][show]})
+      filteredList.push({
+        name: arr[index]["name"],
+        num: arr[index]["num"],
+        data: arr[index][show],
+      });
     }
 
-    
-
     return filteredList.map((dataObject) => (
-      <ToplistRow dataObject={dataObject} show= {showMode}></ToplistRow>
+      <ToplistRow dataObject={dataObject} show={showMode}></ToplistRow>
     ));
   }
 
   function setRedirect(category: string) {
-    var href = "/index"
-    if(category == "Topplista kompetenser") {href = "/kompetenser"}
-    else if(category == "Topplista yrken") {href ="/yrken"}
-    return <Link href={href}>
-    <a className="uppercase font-medium text-gray-500">se alla</a>
-  </Link>
+    var href = "/index";
+    if (category == "Topplista kompetenser") {
+      href = "/kompetenser";
+    } else if (category == "Topplista yrken") {
+      href = "/yrken";
+    }
+    return (
+      <Link href={href}>
+        <a className="uppercase font-medium text-gray-500">se alla</a>
+      </Link>
+    );
   }
 
   return (
-    <div className="flex flex-col pt-8 xl:px-4 py-8" >
-    <div className="-my-2 overflow-hidden sm:-mx-6 lg:-mx-8">
-      <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
-            <tr>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider"
-                >
-                  {category}
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                 
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                 <FilterDropdown initMode="Prognos 12 mån" updateShow = {changeData}></FilterDropdown>
-                </th>
-                
-                
-              </tr>
-              <tr>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-[10px] font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  {title}
-                </th>
-                <th
-                  scope="col"
-                  className="py-3 px-6 text-left text-[10px] font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  <div className="flex flex-row">
-                  Annonser
-                  <button onClick={()=> setSortMode("Alla annonser")}><SortAscendingIcon className="h-5 w-5 text-gray-500 ml-2"/></button>
-                 </div>
-                  
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-[10px] font-medium text-gray-500 uppercase tracking-wider "
-                >
-                  <div className="flex flex-row">{showMode}
-                  <button onClick={()=> setSortMode(showMode)}><SortAscendingIcon className="h-5 w-5 text-gray-500 ml-2"/></button></div>
-                  
-                </th>
-                
-                
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-             
-              {sortBy(data, sortMode, showMode)}
-
-            </tbody>
-            <tr className="bg-gray-50">
+    <div className="flex flex-col pt-8 xl:px-4 py-8">
+      <div className="-my-2 overflow-hidden sm:-mx-6 lg:-mx-8">
+        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-bold text-gray-500 uppercase tracking-wider"
+                  >
+                    {category}
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  ></th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    <FilterDropdown
+                      initMode="Prognos 12 mån"
+                      updateShow={changeData}
+                    ></FilterDropdown>
+                  </th>
+                </tr>
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-[10px] font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    {title}
+                  </th>
+                  <th
+                    scope="col"
+                    className="py-3 px-6 text-left text-[10px] font-medium text-gray-500 uppercase tracking-wider"
+                  >
+                    <div className="flex flex-row">
+                      Annonser
+                      <button onClick={() => setSortMode("Alla annonser")}>
+                        <SortAscendingIcon className="h-5 w-5 text-gray-500 ml-2" />
+                      </button>
+                    </div>
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-left text-[10px] font-medium text-gray-500 uppercase tracking-wider "
+                  >
+                    <div className="flex flex-row">
+                      {showMode}
+                      <button onClick={() => setSortMode(showMode)}>
+                        <SortAscendingIcon className="h-5 w-5 text-gray-500 ml-2" />
+                      </button>
+                    </div>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {sortBy(data, sortMode, showMode)}
+              </tbody>
+              <tr className="bg-gray-50">
                 <td></td>
-                <td className="px-6 py-4">
-                 { setRedirect(category)}
-              
-                </td>
+                <td className="px-6 py-4">{setRedirect(category)}</td>
                 <td></td>
-            </tr>
-          </table>
+              </tr>
+            </table>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
--- pages/yrken/index.tsx
+++ pages/yrken/index.tsx
@@ -4,14 +4,16 @@ import SearchBar from "../../components/SearchBar";
 import Chart from "../../components/Chart";
 import StatsCard from "../../components/StatsCard";
 
+import { mockupData } from "../../lib/mockupData";
+
 const OccupationsOverview: NextPage = () => {
   return (
-    <div className="bg-slate-200 w-full h-full ">
-    <article className="max-w-6xl px-4 mx-auto pt-24 min-h-screen">
-      <SearchBar />
-      <Chart />
-      <StatsCard month={-12} year={26} name="React" />
-    </article>
+    <div className="bg-slate-200 w-full h-full min-h-screen py-12">
+      <article className="max-w-6xl px-4 mx-auto pt-24">
+        <SearchBar placeholder="Sök yrken" />
+        <Chart name="Frontend-utvecklare" data={mockupData} />
+        <StatsCard month={-12} year={26} name="Frontend-utvecklare" />
+      </article>
     </div>
   );
 };
