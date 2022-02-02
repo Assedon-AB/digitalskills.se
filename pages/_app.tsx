01fea7e2a731ddde7054b7705e2b7aadf70bcdfd
Elias Juremalm <eliass.juremalm@gmail.com>
2022-02-02 09:29:16 +0100

    add simple fake data alert

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,9 @@ import Navbar from "../components/Navbar";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <div className="w-full text-center relative top-0 left-0 right-0 py-2 text-sm bg-blue-500 text-white">
+        All data på denna sida är fake data.
+      </div>
       <Navbar />
       <Component {...pageProps} />
     </>
