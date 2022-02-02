1ed8f359ce518a57010735f03b5da55c08ba7967
Elias Juremalm <eliass.juremalm@gmail.com>
2022-02-02 10:31:46 +0100

    add simple start of searchbar test

---
+++ __tests__/SearchBar.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import SearchBar from "../components/SearchBar";
+
+describe("SearchBar", () => {
+  describe("search gets passed placeholder 'Sök kompetenser'", () => {
+    it("Should be rendered with correct placeholder", () => {
+      render(<SearchBar placeholder="Sök kompetenser" />);
+
+      const searchBar = screen.getByPlaceholderText("Sök kompetenser");
+      expect(searchBar).toBeInTheDocument();
+    });
+  });
+});
