1c046efa0a3771a79c02ba096d9bc53791330ec2
Elias Juremalm <eliass.juremalm@gmail.com>
2022-03-09 14:07:26 +0100

    add small card test

---
+++ __tests__/SmallCard.test.tsx
@@ -0,0 +1,38 @@
+import { render, screen } from "@testing-library/react";
+import SmallCard from "../components/SmallCard";
+
+describe("SmallCard", () => {
+  describe("card gets passed href", () => {
+    it("Should render corect text", () => {
+        render(<SmallCard text="React" href="http://localhost/" />);
+
+      const text = screen.getByText("React");
+      expect(text).toBeInTheDocument();
+    });
+
+    it("Should render läs mer link with correct href", () => {
+        render(<SmallCard text="React" href="http://localhost/" />);
+
+      const text = screen.getByText("Läs mer");
+        const href = text.getAttribute("href");
+          expect(text).toBeInTheDocument();
+        expect(href).toEqual("http://localhost/");
+    });
+  });
+
+  describe("card gets passed no href", () => {
+    it("Should render corect text", () => {
+        render(<SmallCard text="React" />);
+
+      const text = screen.getByText("React");
+      expect(text).toBeInTheDocument();
+    });
+
+    it("Should not render läs mer link", () => {
+        render(<SmallCard text="React" />);
+
+      const text = screen.queryByText("Läs mer");
+        expect(text).toBeNull();
+    });
+  });
+});
