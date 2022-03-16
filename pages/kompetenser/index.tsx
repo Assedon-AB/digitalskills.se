2d5d53f852cb495411ca4b4454fc266fc9e1309d
AndreasSamuelsson <andsamuppsala@gmail.com>
2022-03-16 12:36:20 +0100

    fix chart colors

--- pages/kompetenser/index.tsx
+++ pages/kompetenser/index.tsx
@@ -58,10 +58,10 @@ const CompetencesOverview: NextPage<CompetencesPageProps> = ({
     
     
     var name = ""
-    const historyColorsBorder = ["rgb(99, 99, 255)", "rgb(200, 140, 200)"]
-    const historyColorsBackground = ["rgba(99, 99, 255, 0.5)", "rgba(200, 140, 200, 0.5)"]
-    const forecastColorsBorder = ["rgb(255, 99, 132)","rgb(140, 200, 132)"]
-    const forecastColorsBackground = ["rgba(255, 99, 132, 0.5)","rgba(140, 200, 132, 0.5)"]
+    const historyColorsBorder = ["rgb(99, 99, 255)", "rgb(200, 140, 200)", "rgb(100, 10, 220)", "rgb(152, 38, 73)", "rgb(96, 178, 229)"]
+    const historyColorsBackground = ["rgba(99, 99, 255, 0.5)", "rgba(200, 140, 200, 0.5)", "rgb(100, 10, 220, 0.5)", "rgb(152, 38, 73, 0.5)", "rgb(96, 178, 229, 0.5)"]
+    const forecastColorsBorder = ["rgb(255, 99, 132)","rgb(140, 200, 132)", "rgb(220, 50, 132)", "rgb(180, 173, 234)", "rgb(255, 237, 101)"]
+    const forecastColorsBackground = ["rgba(255, 99, 132, 0.5)","rgba(140, 200, 132, 0.5)", "rgb(220, 50, 132, 0.5)","rgb(180, 173, 234, 0.5)", "rgb(255, 237, 101, 0.5)"]
     var finalLables  = compareObjectList[0].ad_series.labels.concat(
       compareObjectList[0].prediction_series.month_12.labels
     )
