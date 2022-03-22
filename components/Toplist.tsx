1cd813b39a44c82eaf4d1e3e114c004165d348b8
Elias Juremalm <eliass.juremalm@gmail.com>
2022-03-22 14:13:39 +0100

    make toplist scrollable

--- components/Toplist.tsx
+++ components/Toplist.tsx
@@ -157,8 +157,8 @@ const Toplist = ({ data, title, category, industry }: ToplistProps) => {
   return (
     <div className="flex flex-col pt-8 xl:px-4 py-8">
       <div className="-my-2 overflow-hidden sm:-mx-6 lg:-mx-8">
-        <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-          <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+        <div className="py-2 align-middle min-w-full sm:px-6 lg:px-8">
+          <div className="shadow overflow-auto border-b border-gray-200 sm:rounded-lg">
             <table className="min-w-full divide-y divide-gray-200">
               <thead className="bg-gray-50">
                 <tr>
