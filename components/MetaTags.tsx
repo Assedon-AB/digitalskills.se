8637cd40863410d028378f2500280926d330ac27
Elias Juremalm <eliass.juremalm@gmail.com>
2022-03-30 10:30:09 +0200

    rebrand from digitalspetskompetens to digitalskills

--- components/MetaTags.tsx
+++ components/MetaTags.tsx
@@ -3,19 +3,19 @@ import Head from "next/head";
 const MetaTags = ({title}: {title: string}) => {
     return (
         <Head>
-            <title>{title + " - Digitalspetskompetens"}</title>
-            <meta name="title" content={title + " - Digitalspetskompetens"} />
+            <title>{title + " - Digitalskills.se"}</title>
+            <meta name="title" content={title + " - Digitalskills.se"} />
             <meta name="description" content="Tillväxtverket och Universitetskanslersämbetet har av regeringen fått i uppdrag att tillsammans analysera och föreslå hur kompetensförsörjningen av digital spetskompetens kan utvecklas både kort- och långsiktigt. Vi ska starta en dialog om samverkan mellan berörda aktörer, i syfte att öka tillgången på digital spetskompetens."/>
 
             <meta property="og:type" content="website"/>
             <meta property="og:url" content="https://digspec-fe.vercel.app/"/>
-            <meta property="og:title" content={title + " - Digitalspetskompetens"}/>
+            <meta property="og:title" content={title + " - Digitalskills.se"}/>
             <meta property="og:description" content="Tillväxtverket och Universitetskanslersämbetet har av regeringen fått i uppdrag att tillsammans analysera och föreslå hur kompetensförsörjningen av digital spetskompetens kan utvecklas både kort- och långsiktigt. Vi ska starta en dialog om samverkan mellan berörda aktörer, i syfte att öka tillgången på digital spetskompetens."/>
             <meta property="og:image" content="/digitalspetskompetens-meta.png"/>
 
             <meta property="twitter:card" content="summary_large_image"/>
             <meta property="twitter:url" content="https://digspec-fe.vercel.app/"/>
-            <meta property="twitter:title" content={title + " - Digitalspetskompetens"}/>
+            <meta property="twitter:title" content={title + " - Digitalskills.se"}/>
             <meta property="twitter:description" content="Tillväxtverket och Universitetskanslersämbetet har av regeringen fått i uppdrag att tillsammans analysera och föreslå hur kompetensförsörjningen av digital spetskompetens kan utvecklas både kort- och långsiktigt. Vi ska starta en dialog om samverkan mellan berörda aktörer, i syfte att öka tillgången på digital spetskompetens."/>
             <meta property="twitter:image" content="/digitalspetskompetens-meta.png"/>
 
