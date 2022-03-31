6bf2c01705cd872da129c4801d92c02e9bdd1e33
Elias Juremalm <eliass.juremalm@gmail.com>
2022-03-31 12:22:51 +0200

    add favicons

--- components/MetaTags.tsx
+++ components/MetaTags.tsx
@@ -19,6 +19,10 @@ const MetaTags = ({title}: {title: string}) => {
             <meta property="twitter:description" content="Tillväxtverket och Universitetskanslersämbetet har av regeringen fått i uppdrag att tillsammans analysera och föreslå hur kompetensförsörjningen av digital spetskompetens kan utvecklas både kort- och långsiktigt. Vi ska starta en dialog om samverkan mellan berörda aktörer, i syfte att öka tillgången på digital spetskompetens."/>
             <meta property="twitter:image" content="/digitalspetskompetens-meta.png"/>
 
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="manifest" href="/site.webmanifest"/>
         </Head>
     );
 }
