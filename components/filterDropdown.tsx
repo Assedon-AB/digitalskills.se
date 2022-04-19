fac8d28b881536d10af5c7a9c147e76a6a64f374
Elias Juremalm <eliass.juremalm@gmail.com>
2022-04-19 12:20:44 +0200

    fix filter z-index

--- components/filterDropdown.tsx
+++ components/filterDropdown.tsx
@@ -29,7 +29,7 @@ const FilterDropdown = ({ initMode, updateShow }: FilterDropdownProps) => {
   }, [mode]);
 
   return (
-    <Menu as="div" className="relative inline-block text-left w-40 ">
+    <Menu as="div" className="relative inline-block text-left w-40 z-20">
       <div>
         <Menu.Button className="inline-flex justify-between w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-xs font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
           {mode}
