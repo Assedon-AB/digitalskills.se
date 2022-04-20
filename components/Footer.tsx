82c41a4385bc749b2fff3bc335b41b977d8cb548
Elias Juremalm <eliass.juremalm@gmail.com>
2022-04-20 13:32:37 +0200

    add suggestions/bug mailto link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <footer className="bg-[#fafafa] p-8">
+        <p className="text-center mb-4">Har du hittat en bugg eller har förslag på förbättringar? <a href="mailto:fred@assedon.se?subject=Bugg eller förslag digitalskills.se" className="underline hover:text-blue-500">Kontakta oss</a></p>
       <div className="mx-auto max-w-6xl flex flex-col md:flex-row justify-between">
         <div className="md:w-3/5 md:mr-8">
           <p className="text-xl mb-2">{"digitalskills.se"}</p>
