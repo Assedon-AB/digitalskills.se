4b25bec7e0806333c43392acd27b242163b99507
Elias Juremalm <eliass.juremalm@gmail.com>
2023-02-06 17:57:12 +0100

    update data

--- components/AttentionCard.tsx
+++ components/AttentionCard.tsx
@@ -17,8 +17,8 @@ const AttentionCard = () => {
 						>
 							historiska jobbannonser från Platsbanken
 						</a>
-						. Hämtades senast 2022-11-23 och sträcker sig mellan
-						2006-01-01 till 2022-09-30.
+						. Hämtades senast 2023-02-05 och sträcker sig mellan
+						2006-01-01 till 2022-12-31.
 					</p>
 					<h2 className="font-medium text-sm leading-relaxed pt-4">
 						Så tolkar du tabellen
